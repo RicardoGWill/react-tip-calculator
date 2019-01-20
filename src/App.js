93498f90d48c7455335a6e6c5e45dc96f001250c
ricardogwill <ricardogwill@gmail.com>
2019-01-20 01:20:38 -0500

    Header, Body, and Footer basics set up.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './components/Header';
+import Body from './components/Body';
+import Footer from './components/Footer';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <div className="Header-and-Body">
+          <div className="App-header">
+            <Header />
+          </div>
+          <div>
+            <Body />
+          </div>
+        </div>
+        <div className="App-footer">
+          <Footer />
+        </div>
       </div>
     );
   }
