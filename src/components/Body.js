f2f7c05f8b3c493be022f661b5f500464073f388
ricardogwill <ricardogwill@gmail.com>
2019-01-31 01:30:18 -0500

    Added a second form. (Neither are finished.)

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,14 +4,23 @@ class Body extends Component {
   render(){
     return(
       <div>
+        <h3>First Form</h3>
         <form>
           <label>
             Bill Amount:
             <input type="text" name="billAmount" />
           </label>
           <input type="submit" value="Submit" />
-          <h1>Next</h1>
         </form>
+        <h3>Second Form</h3>
+        <form>
+          <label>
+            Percentage Tip:
+            <input type="text" name="PercentageTip" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <h3>Total: </h3>
       </div>
     );
   }
