37d1dab8a4d7c87d6f94ebda14bf1c2204bbec15
ricardogwill <ricardogwill@gmail.com>
2019-02-02 02:49:39 -0500

    Two text input fields are working, but the total is not working yet.

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,22 +5,38 @@ class Body extends Component {
     super(props);
 
     this.state = {
-      total: null
+      billAmount: "",
+      percentageTip: "",
+      total: ""
     };
   }
 
-  handleChange = (event) => {
+  handleChangeBillAmount = (event) => {
     this.setState({
-      total: event.target.value
+      billAmount: event.target.value
     });
   };
+
+  // Check out "Numeric Input" for React (https://www.npmjs.com/package/react-numeric-input).
+  handleChangePercentageTip = (event) => {
+    this.setState({
+      percentageTip: event.target.value
+    });
+  };
+
+  handleSubmit(event) {
+    // event.preventDefault();
+    // this.setState({
+    //   total: this.state.billAmount + this.state.percentageTip
+    });
+  }
 
 
   render(){
     return(
       <div>
-        <h3>First Form</h3>
-        <form>
+        <h3>Form</h3>
+        <form onSubmit={this.handleSubmit.bind(this)}>
           <div>
            <label>
              Bill Amount:
@@ -28,20 +44,26 @@ class Body extends Component {
               type="text"
               name="billAmount"
               placeholder="$"
-              value={this.state.total}
-              onChange={this.handleChange.bind(this)} />
+              value={this.state.billAmount}
+              onChange={this.handleChangeBillAmount.bind(this)} />
            </label>
           </div>
           <div>
            <label>
              Percentage Tip:
-             <input type="text" name="PercentageTip" placeholder="%" />
+             <input type="text"
+             name="PercentageTip"
+             placeholder="%"
+             value={this.state.percentageTip}
+             onChange={this.handleChangePercentageTip.bind(this)} />
            </label>
           </div>
           <div>
             <input type="submit" value="Submit" />
           </div>
         </form>
+        <h3>Bill Amount: ${this.state.billAmount}</h3>
+        <h3>Percentage Tip: {this.state.percentageTip}%</h3>
         <h3>Total: ${this.state.total}</h3>
       </div>
     );
